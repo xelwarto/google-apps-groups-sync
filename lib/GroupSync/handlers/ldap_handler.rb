023574d89a9cb2c629be2de4f8b8acb89a9d76f8
Ted Elwartowski <xelwarto.pub@gmail.com>
2014-05-02 09:56:40 -0500

    Updated the LdapHandler class to support the new configuration methods and options - removed any static refrence to LDAP parameters

--- lib/GroupSync/handlers/ldap_handler.rb
+++ lib/GroupSync/handlers/ldap_handler.rb
@@ -17,21 +17,21 @@ module Google::GroupSync
     attr_reader :configured
     
     def initialize
-      @cfg = Google::GroupSync::Config.instance.get
+      @config = Google::GroupSync::Config.instance.config.ldap
+      @search = @config.search
       @log = Google::GroupSync::Log.instance
       @log.info 'LdapHandler:Initializing LDAP Handler'
       
       @configured = false
-      @base = @cfg[:ldap][:base]
       
       auth = { 
         :method => :simple,
-        :username => @cfg[:ldap][:adm_dn],
-        :password => @cfg[:ldap][:adm_pass] }
-      @ldap = Net::LDAP.new :host => @cfg[:ldap][:server], :port => @cfg[:ldap][:port], :auth => auth
+        :username => @config.bind_dn,
+        :password => @config.bind_pass }
+      @ldap = Net::LDAP.new :host => @config.server, :port => @config.port, :auth => auth
       
       begin
-        Timeout::timeout(@cfg[:ldap][:timeout]) do
+        Timeout::timeout(@config.timeout) do
           if @ldap.bind
             @configured = true
           end
@@ -43,7 +43,7 @@ module Google::GroupSync
       end
     end
     
-    def ent_from_dn(dn=nil,attrs=['objectClass','mail'],&block)
+    def ent_from_dn(dn=nil,attrs=['objectClass'],&block)
       @log.debug 'LdapHandler(ent_from_dn):Searching for LDAP entity from DN'
       if !dn.nil? && dn != ''
         filter = Net::LDAP::Filter.eq :objectClass, 'top'
@@ -53,84 +53,118 @@ module Google::GroupSync
       end
     end
     
-    def get_std_groups(attrs=['cn','mail','uniqueMember','owner', 'description', 'mailAlternateAddress'],&block)
-      @log.debug 'LdapHandler(get_std_groups):Searching for LDAP groups'
-      filter = Net::LDAP::Filter.eq :objectClass, 'groupofuniquenames'
-      run_handler @base, filter, attrs, &block
+    def search(base=nil,filter=nil,attrs=['objectClass'],&block)
+      @log.debug 'LdapHandler(search):Performing LDAP Search'
+      if !base.nil?
+        if !filter.nil?
+          filter = Net::LDAP::Filter.construct filter
+          run_handler base, filter, attrs, &block
+        else
+          @log.error 'LdapHandler(search):Search filter is invalid'
+        end
+      else
+        @log.error 'LdapHandler(search):Search base is invalid'
+      end
     end
     
-    def get_all_users(attrs=['uid','mail'],&block)
-      @log.debug 'LdapHandler(get_std_groups):Searching for all LDAP users'
-      filter = Net::LDAP::Filter.eq :objectClass, 'person'
-      run_handler 'CHANGE HERE', filter, attrs, &block
-    end
-    
-    def get_ldap_groups
-      @log.debug 'LdapHandler(get_ldap_groups):Running LDAP query to get groups'
+    def get_groups
+      @log.debug 'LdapHandler(get_groups):Running LDAP query to get groups'
       groups = Hash.new
       
+      attrs = ['objectClass']
+      attrs.push @search.groups_name_attr
+      attrs.push @search.groups_mail_attr
+      attrs.push @search.groups_member_attr
+      
+      if !@search.groups_owner_attr.nil? && !@search.groups_owner_attr.eql ''
+        attrs.push @search.groups_owner_attr
+      end
+      if !@search.groups_manager_attr.nil? && !@search.groups_manager_attr.eql ''
+        attrs.push @search.groups_manager_attr
+      end
+      if !@search.groups_descr_attr.nil? && !@search.groups_descr_attr.eql ''
+        attrs.push @search.groups_descr_attr
+      end
+      if !@search.groups_alias_attr.nil? && !@search.groups_alias_attr.eql ''
+        attrs.push @search.groups_alias_attr
+      end
+      
       begin
-        get_std_groups do |ent,ldap|
+        search @search.groups_base, @search.groups_filter, attrs do |ent,ldap|
           if !ent.nil?
             grp = Hash.new
             grp[:ent] = ent
             grp[:members] = Array.new
             grp[:owners] = Array.new
             
-            if !ent['cn'].nil? && ent['cn'].any?
-              grp[:cn] = ent['cn'].first
-              @log.debug "LdapHandler(get_ldap_groups):Found LDAP group: #{grp[:cn]}"
+            if !ent[@search.groups_name_attr].nil? && ent[@search.groups_name_attr].any?
+              grp[:name] = ent[@search.groups_name_attr].first
+              @log.debug "LdapHandler(get_groups):Found LDAP group: #{grp[:name]}"
               
-              if !ent['mail'].nil? && ent['mail'].any?
-                grp[:mail] = ent['mail'].first.downcase
+              if !ent[@search.groups_mail_attr].nil? && ent[@search.groups_mail_attr].any?
+                grp[:mail] = ent[@search.groups_mail_attr].first.downcase
                 
-                if !ent['mailAlternateAddress'].nil? && ent['mailAlternateAddress'].any?
-                  grp[:mailAlternateAddress] = ent['mailAlternateAddress']
+                if !@search.groups_alias_attr.nil? && !@search.groups_alias_attr.eql ''
+                  if !ent[@search.groups_alias_attr].nil? && ent[@search.groups_alias_attr].any?
+                    grp[:alias] = ent[@search.groups_alias_attr]
+                  end
                 end
                 
-                if !ent['description'].nil? && ent['description'].any?
-                  grp[:description] = ent['description'].first
+                if !@search.groups_manager_attr.nil? && !@search.groups_manager_attr.eql ''
+                  if !ent[@search.groups_manager_attr].nil? && ent[@search.groups_manager_attr].any?
+                    grp[:manager] = ent[@search.groups_manager_attr]
+                  end
+                end
+                
+                if !@search.groups_descr_attr.nil? && !@search.groups_descr_attr.eql ''
+                  if !ent[@search.groups_descr_attr].nil? && ent[@search.groups_descr_attr].any?
+                    grp[:description] = ent[@search.groups_descr_attr].first
+                  else
+                    grp[:description] = grp[@search.groups_name_attr.to_sym]
+                  end
                 else
-                  grp[:description] = grp[:cn]
+                  grp[:description] = grp[@search.groups_name_attr.to_sym]
                 end
                 
                 groups[grp[:mail].downcase.to_sym] = grp
               else
-                @log.error "LdapHandler(get_ldap_groups):LDAP group does not have valid email address - skipping: #{grp[:cn]}"
+                @log.error "LdapHandler(get_groups):LDAP group does not have valid email address - skipping: #{grp[:name]}"
               end
             end
           end
         end
       rescue Exception => e
-        @log.error "App(get_ldap_groups):#{e}"
+        @log.error "App(get_groups):#{e}"
         groups = nil
       end
       
       groups
     end
     
-    def get_ldap_users
-      @log.debug 'LdapHandler(get_ldap_users):Running LDAP query to get users'
+    def get_users
+      @log.debug 'LdapHandler(get_users):Running LDAP query to get users'
       users = Hash.new
       
+      attrs = ['objectClass']
+      attrs.push @search.groups_mail_attr
       begin
-        get_all_users do |ent,ldap|
+        search @search.users_base, @search.users_filter, attrs do |ent,ldap|
           if !ent.nil?
             dn = ent[:dn].first
             dn.downcase!
             dn.delete! "\s"
             dn.strip!
             
-            if !ent[:mail].nil? && ent[:mail].any?
+            if !ent[@search.groups_mail_attr].nil? && ent[@search.groups_mail_attr].any?
               user = Hash.new
-              user[:mail] = ent[:mail].first.downcase.to_s
+              user[:mail] = ent[@search.groups_mail_attr].first.downcase.to_s
               user[:type] = 'USER'
               users[dn.to_sym] = user
             end
           end
         end
       rescue Exception => e
-        @log.error "LdapHandler(get_ldap_users):#{e}"
+        @log.error "LdapHandler(get_users):#{e}"
         users = nil
       end
       
